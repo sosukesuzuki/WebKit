8d2faa6e34d4a36f5ed83077a1779f489ff3cfe2
sosukesuzuki <aosukeke@gmail.com>
2025-03-09 20:00:03 +0900

    [JSC] Handle `String#replaceAll` in DFG/FTL https://bugs.webkit.org/show_bug.cgi?id=276847
    Reviewed by NOBODY (OOPS!).
    
    Currently, JSC has implemented DFG/FTL support for String#replace, but there is no support for
    String#replaceAll.
    
    This patch changes String#replaceAll to be handled by DFG/FTL same as String#replace.
    
    The benchmark results are as follows. When the search is a RegExp object, it is about 1.7 ~ 1.8x
    faster in microbenchmarks:
    
                                                      TipOfTree                  Patched
    
    string-replaceall-regexp-empty-str-benchmark
                                                  231.7543+-1.8543     ^    126.4888+-1.2796        ^ definitely 1.8322x faster
    string-replaceall-string-string-benchmark
                                                  402.9816+-13.6192         393.6110+-2.1591          might be 1.0238x faster
    string-replaceall-regexp-generic-benchmark
                                                  281.7777+-5.9623     ^    159.4750+-0.5618        ^ definitely 1.7669x faster
    string-replaceall-regexp-string-benchmark
                                                  257.2394+-2.3629     ^    142.2257+-5.0849        ^ definitely 1.8087x faster
    
    * Source/JavaScriptCore/builtins/StringPrototype.js:
    (replaceAll): Deleted.
    * Source/JavaScriptCore/dfg/DFGAbstractInterpreterInlines.h:
    (JSC::DFG::AbstractInterpreter<AbstractStateType>::executeEffects):
    * Source/JavaScriptCore/dfg/DFGByteCodeParser.cpp:
    (JSC::DFG::ByteCodeParser::handleIntrinsicCall):
    * Source/JavaScriptCore/dfg/DFGClobberize.h:
    (JSC::DFG::clobberize):
    * Source/JavaScriptCore/dfg/DFGDoesGC.cpp:
    (JSC::DFG::doesGC):
    * Source/JavaScriptCore/dfg/DFGFixupPhase.cpp:
    (JSC::DFG::FixupPhase::fixupNode):
    * Source/JavaScriptCore/dfg/DFGNode.h:
    (JSC::DFG::Node::hasHeapPrediction):
    * Source/JavaScriptCore/dfg/DFGNodeType.h:
    * Source/JavaScriptCore/dfg/DFGPredictionPropagationPhase.cpp:
    * Source/JavaScriptCore/dfg/DFGSafeToExecute.h:
    (JSC::DFG::safeToExecute):
    * Source/JavaScriptCore/dfg/DFGSpeculativeJIT.cpp:
    * Source/JavaScriptCore/dfg/DFGSpeculativeJIT.h:
    * Source/JavaScriptCore/dfg/DFGSpeculativeJIT32_64.cpp:
    (JSC::DFG::SpeculativeJIT::compile):
    * Source/JavaScriptCore/dfg/DFGSpeculativeJIT64.cpp:
    (JSC::DFG::SpeculativeJIT::compile):
    * Source/JavaScriptCore/dfg/DFGStrengthReductionPhase.cpp:
    (JSC::DFG::StrengthReductionPhase::handleNode):
    * Source/JavaScriptCore/ftl/FTLCapabilities.cpp:
    (JSC::FTL::canCompile):
    * Source/JavaScriptCore/runtime/Intrinsic.h:

--- Source/JavaScriptCore/ftl/FTLLowerDFGToB3.cpp
+++ Source/JavaScriptCore/ftl/FTLLowerDFGToB3.cpp
@@ -1748,6 +1748,7 @@ private:
             compileSetFunctionName();
             break;
         case StringReplace:
+        case StringReplaceAll:
         case StringReplaceRegExp:
             compileStringReplace();
             break;
@@ -17147,7 +17148,7 @@ IGNORE_CLANG_WARNINGS_END
                     LValue string = lowString(m_node->child1());
                     LValue regExp = lowRegExpObject(m_node->child2());
 
-                    LValue result = vmCall(pointerType(), operationStringProtoFuncReplaceRegExpEmptyStr, weakPointer(globalObject), string, regExp);
+                    LValue result = vmCall(pointerType(), m_node->op() == StringReplaceAll ? operationStringProtoFuncReplaceAllRegExpEmptyStr : operationStringProtoFuncReplaceRegExpEmptyStr, weakPointer(globalObject), string, regExp);
 
                     setJSValue(result);
                     return;
@@ -17158,7 +17159,7 @@ IGNORE_CLANG_WARNINGS_END
             LValue regExp = lowRegExpObject(m_node->child2());
             LValue replace = lowString(m_node->child3());
 
-            LValue result = vmCall(pointerType(), operationStringProtoFuncReplaceRegExpString, weakPointer(globalObject), string, regExp, replace);
+            LValue result = vmCall(pointerType(), m_node->op() == StringReplaceAll ? operationStringProtoFuncReplaceAllRegExpString : operationStringProtoFuncReplaceRegExpString, weakPointer(globalObject), string, regExp, replace);
 
             setJSValue(result);
             return;
@@ -17171,7 +17172,8 @@ IGNORE_CLANG_WARNINGS_END
             search = lowJSValue(m_node->child2());
 
         LValue result = vmCall(
-            pointerType(), operationStringProtoFuncReplaceGeneric,
+            pointerType(),
+            m_node->op() == StringReplaceAll ? operationStringProtoFuncReplaceAllGeneric : operationStringProtoFuncReplaceGeneric,
             weakPointer(globalObject),
             lowJSValue(m_node->child1()), search,
             lowJSValue(m_node->child3()));
